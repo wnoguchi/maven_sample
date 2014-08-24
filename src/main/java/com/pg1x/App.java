afc7ea2f93640772733b7b6d1f43791b1c097733
Wataru Noguchi <wnoguchi.0727@gmail.com>
2014-08-24 12:48:21 +0900

    Initial commit.

---
+++ src/main/java/com/pg1x/App.java
@@ -0,0 +1,18 @@
+package com.pg1x;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+		public static int sum(int a, int b)
+		{
+				return a + b;
+		}
+}
